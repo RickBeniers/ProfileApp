b5c3b663f76bb051d842fcdbd21162049cc45cb5
ZoruaStar64 <sanderb119@gmail.com>
2023-10-17 17:26:49 +0200

    quick update
    footer is now properly called from the partials/footer.php, styling of tablet/desktop version has been slightly changed to have less whitespace between the input fields and added some helpfull text telling people which fields are required (also made the standard value of the date input 2000-01-01 so people don't have  to keep scrolling down from 2023)

--- views/newAccount.view.php
+++ views/newAccount.view.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../stylesheets/nav.css">
     <link rel="stylesheet" href="../stylesheets/footer.css">
     <link rel="stylesheet" href="../stylesheets/register.css">
-    <title>Register - profileApp</title>
+    <title>Register - ProfielPlus</title>
 </head>
 <body>
     <header>
@@ -20,24 +20,24 @@
         <div class="registerContainer">
             <div class="centerText">
                 <h1>Create your Profielplus account</h1>
+                <p>a * inside of the input fields below means that it's required.</p>
+                <b>The date of birth input field is also required (it's the one with a calender icon)</b>
             </div>
             <form action="../functions/register.php" method="post"  class="registerForm">
-                <input type="text" placeholder="Enter your first name." name="first_name" id="first_name" class="registerField" value="" required>
+                <input type="text" placeholder="Enter your first name. *" name="first_name" id="first_name" class="registerField" value="" required>
                 <input type="text" placeholder="Enter your name's insertion." name="insertion" id="insertion" class="registerField" value="">
-                <input type="text" placeholder="Enter your last name." name="last_name" id="last_name" class="registerField" value="" required>
-                <input type="text" placeholder="Enter your unique username." name="username" id="username" class="registerField" value="" required>
-                <input type="email" placeholder="Enter your email adress." name="e-mail" id="e-mail" class="registerField" value="" required>
-                <input type="password" placeholder="Enter your password" name="password" id="password" class="registerField" value="" required>
-                <input type="password" placeholder="Re-enter your password" name="confirm_password" id="confirm_password" class="registerField" value="" required>
-                <input type="date" placeholder="Please enter your date of birth" name="date_of_birth" id="date_of_birth" class="registerField" value="" required>
+                <input type="text" placeholder="Enter your last name. *" name="last_name" id="last_name" class="registerField" value="" required>
+                <input type="text" placeholder="Enter your unique username. *" name="username" id="username" class="registerField" value="" required>
+                <input type="email" placeholder="Enter your email adress. *" name="e-mail" id="e-mail" class="registerField" value="" required>
+                <input type="password" placeholder="Enter your password. *" name="password" id="password" class="registerField" value="" required>
+                <input type="password" placeholder="Re-enter your password. *" name="confirm_password" id="confirm_password" class="registerField" value="" required>
+                <input type="date" placeholder="Please enter your date of birth. *" name="date_of_birth" id="date_of_birth" class="registerField" value="2000-01-01" required>
                 <input type="submit" name="create_account" id="create_account" class="registerButton" value="Create Account">
             </form>
         </div>
     </main>
     <footer>
-        <div class="footerText">
-            <p>&copy; Team PSR 2023</p>
-        </div>
+        <?php require 'partials/footer.php';?>
     </footer>
 </body>
 </html>
