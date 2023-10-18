09f37d5d4f825b80e720d6624bce620bd8041f1a
paulroc <54845508+paulroc@users.noreply.github.com>
2023-10-18 12:27:34 +0200

    Inloggen getest

---
+++ private.php
@@ -0,0 +1,14 @@
+<?php
+
+
+return [
+    "database" => [
+        "servername" => "localhost",
+        "usernameOrEmail" => "root",
+        "password" => "Blub!976",
+        "dbname" => "ProfilePlus"
+    ],
+    "information" => [
+        "email" => "info@windesheim.nl"
+    ]
+];
