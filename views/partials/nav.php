5ab30cbf269c953d84821ed0a02aeac477e0901d
ZoruaStar64 <sanderb119@gmail.com>
2023-10-18 12:28:50 +0200

    slight changes to nav
    also uploading the connection and placeholder db creds so a template for it is easily available (home button is also now a part of the navbar and it's original position has been replaced with a page title text)

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -4,13 +4,16 @@ $_SESSION['loggedin'] = false;
 ?>
 
 <nav class="navbar">
-    <div class="homeButton">
-        <a href="./">Home</a>
+    <div class="pageTitle">
+        <!--Replace the Placeholder text with the actual page title-->
+        <h2>Placeholder</h2>
     </div>
     <div class="navButtons">
+        <a id="homeButton" class="navButton" href="./">Home</a>
         <?php if (!$_SESSION['loggedin']) {
             echo '<a id="registerButton" class="navButton" href="./register">Register</a>
                 <a id="loginButton" class="navButton" href="./login">Login</a>';
+
         } else {
             echo '<a id="myPortfolioButton" class="navButton" href="./myPortfolio">My Portfolio</a>
                 <a id="logoutButton" class="navButton" href="./logout">Logout</a>';
