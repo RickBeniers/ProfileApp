982d16fcc2de6ec0ce11030892fcb643c5c0fccb
paulroc <54845508+paulroc@users.noreply.github.com>
2023-11-02 15:02:26 +0100

    Delete knop

---
+++ admin.js
@@ -0,0 +1,5 @@
+function removeAccount() {
+    var username = document.getElementById("username").value;
+    // Voer hier de logica uit om het account van de gebruiker te ontzeggen, bijv. een API-aanroep naar de back-end.
+    alert("Account voor gebruiker " + username + " is ontzegd.");
+}
