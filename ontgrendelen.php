6477cee3d7d58322c5e8f95c84f9249f5afa2754
ZoruaStar64 <sanderb119@gmail.com>
2023-11-03 21:13:42 +0100

    Final update to accounts_beheren
    tested the functions and they work (the db mysqli connection just needs your database credentials and you're good to go
    also changed the path of the adminPanelButton to go to ../admin.php instead of ../controllers/adminPanel.php

--- ontgrendelen.php
+++ ontgrendelen.php
@@ -4,6 +4,15 @@ if (isset($_GET['id'])) {
     $gebruikerId = $_GET['id'];
 
     // Verbinding maken met de database (herhaal de verbindingscode hier)
+    $dbHost = 'localhost'; // De host van je database
+    $dbGebruikersnaam = 'username'; // Je database gebruikersnaam
+    $dbWachtwoord = 'raspywords'; // Je database wachtwoord
+    $dbNaam = 'profielplus'; // Naam van je database
+    $mysqli = new mysqli($dbHost, $dbGebruikersnaam, $dbWachtwoord, $dbNaam);
+
+    if ($mysqli->connect_error) {
+        die('Verbindingsfout: ' . $mysqli->connect_error);
+    }
 
     // Query om de gebruiker te ontgrendelen
     $query = "UPDATE users SET is_deleted = 0 WHERE id = $gebruikerId";
