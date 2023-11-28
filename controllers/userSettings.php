fec3b4729e01e13377e45f57c0f724ec1887df88
paulroc <54845508+paulroc@users.noreply.github.com>
2023-11-28 11:21:34 +0100

    Nog meer comments

--- controllers/userSettings.php
+++ controllers/userSettings.php
@@ -1,3 +1,13 @@
 <?php
 
 require '../views/settings.view.php';
+
+//eze PHP-code laadt een ander bestand in, namelijk settings.view.php, dat zich vermoedelijk in de map 'views' bevindt en één niveau hoger dan de huidige map waarin deze PHP-code zich bevindt.
+//
+//Hier is een gedetailleerde uitleg:
+//
+//<?php: Dit opent een PHP-codeblok.
+//
+//require '../views/settings.view.php';: Dit is een zogenaamde 'require'-instructie. Het wordt gebruikt om een ander bestand in de huidige PHP-code in te voegen. In dit geval wordt het bestand settings.view.php ingevoegd. Het gebruik van '../' betekent dat het bestand zich één niveau hoger in de mapstructuur bevindt. Het pad kan variëren afhankelijk van de directorystructuur van je project.
+//
+//?>: Dit sluit het PHP-codeblok.
